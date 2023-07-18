30e5a96f137bbe7ce58987e277bf3ba077477e85
GAOV314 <131786737+GAOV314@users.noreply.github.com>
2023-07-17 21:25:58 -0600

    Primer commit 17/7/23
    Hice hasta el 14.

--- ProyectoCrossy/MyWorld.java
+++ ProyectoCrossy/MyWorld.java
@@ -13,9 +13,41 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    int [][] map= 
+               {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(750, 750, 1);
+        for(int row= 0; row<16; row++)
+            for(int col =0; col <16; col++)
+                if (map [row][col] == 0)
+                    addObject(new Image("Proyecto.png"),col * 50,row * 50 );
+                else if(map[row][col] == 1]);
+                    addObject(new Image(""), col*50, row*50);
+                //else if(map[row][col] == 2])
+                //else if(map[row][col] == 3])
+                //else if(map[row][col] == 4])
+                //else if(map[row][col] == 5])
+                //else if(map[row][col] == 6])
+                //else if(map[row][col] == 7])
+                //else if(map[row][col] == 8])
+
+            
     }
 }
